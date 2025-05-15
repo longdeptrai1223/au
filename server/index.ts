f5d544949e788736312984e5249f8021e26a4d24
Long216795 <longgat930@gmail.com>
2025-05-15 13:47:47 +0700

    first commit

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,10 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
