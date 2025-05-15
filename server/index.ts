86739b240097d92c9d1e59aa093035330a5a64b3
Long216795 <longgat930@gmail.com>
2025-05-15 08:09:40 +0700

    chỉnh sửa server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
-const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
