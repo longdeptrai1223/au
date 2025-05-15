5ff70b00393fcbc9624416e16c4306dd13ed5538
Long216795 <longgat930@gmail.com>
2025-05-15 19:50:51 +0700

    sửa lỗi vite

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,12 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist/client"),
     emptyOutDir: true,
   },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
 });
